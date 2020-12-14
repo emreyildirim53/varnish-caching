978363fd3996f935c310411b18775870cdf527ec
Emre Yıldırım <emre.yildirim@sahibinden.com>
2020-12-14 18:07:59 +0300

    created demo spring boot project / url waiting with 5s delay

---
+++ application/src/test/java/com/example/varnish/demo/VarnishDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.varnish.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarnishDemoApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
